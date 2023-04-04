8f6c9766a4004197f8ba34016cefcb6a2cddd242
Laura Brea <laurabrea17@gmail.com>
2023-04-04 17:21:28 -0400

    could not get the background to show properly and the bootstrap buttons were not showing correctly either

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Background from './components/Background';
-import TopBar from './components/TopBar';
+import "./App.css";
+import Background from "./components/Background";
+import TopBar from "./components/TopBar";
+import StoreListings from "./components/StoreListing";
 
 function App() {
   return (
     <div>
-     <TopBar />
-     <Background />
+      <TopBar />
+      <Background />
+      <StoreListings />
     </div>
   );
 }
